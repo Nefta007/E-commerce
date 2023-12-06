fc11c5f3d98a7835871842e99a17d13466285f5b
Neftali <nperc001@ucr.edu>
2023-12-06 09:55:27 -0800

    made some alterations to tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,97 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attribute:["id", "tag_name"],
+    include:[{
+      model: Product,
+      attribute: ["product_name", "price", "stock", "category_id"],
+    }]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  findOne({
+    where:{
+      id:req.params.id
+    },
+    attribute:["id", "tag_name"],
+    include:[{
+      model: Product,
+      attribute: ["id", "product_name", "price", "stock", "category_id"],
+      through: ProductTag,
+      as: "products",
+    },],
+  })
+  .then(tagData=>{
+    if(!tagData){
+      res.status(404).json({message: "There is no such ID"});
+      return;
+    }res.json(tagData);
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err=>{
+    console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body,{
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(tagData=>{
+    if(!tagData[0]){
+      res.status(404).json({message: "There is no such ID"});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id,
+    },
+  })
+  .then(tagData=>{
+    if(!tagData){
+      res.status(404).json({message:"There is no such Id"});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
